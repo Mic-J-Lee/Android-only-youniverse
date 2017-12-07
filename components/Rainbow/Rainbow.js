f0a37f26ebad029b03d8f6df6f228d47e1800c40
Michael Lee <mleenavy@gmail.com>
2017-12-07 10:50:30 -0500

    faded

--- components/Rainbow/Rainbow.js
+++ components/Rainbow/Rainbow.js
@@ -5,13 +5,18 @@ import Stripe from './Stripe'
 export default class Rainbow extends Component {
   render() {
     const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+    const fadedRainbow = ['#ff9999', '#ffdb99', '#ffff99', '#99ffa2', '#adadff', '#ffa3ff']
     let allStripes = []
-    for (color of rainbow) {
+    for (let i = 0; i < 6; i++) {
+      let color = rainbow[i]
+      let fadedColor = fadedRainbow[i]
+      let isEnabled = this.props.rainbowStatus[color]
       allStripes.push(
         <Stripe key={color} 
-                color={color} 
-                active={this.props.activeColor == color}
-                enabled={this.props.rainbowStatus[color] == true}
+                identity={color}
+                color={isEnabled == true ? color : fadedColor} 
+                isActive={this.props.activeColor == color}
+                isEnabled={isEnabled}
                 _toggleStripe={this.props._toggleStripe} />
       )
     }
--- components/Rainbow/Stripe.js
+++ components/Rainbow/Stripe.js
@@ -6,9 +6,10 @@ export default class Stripe extends Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        <TouchableOpacity onPress={() => this.props._toggleStripe(this.props.color)} 
-                          style={{flex: 4, backgroundColor: this.props.enabled ? this.props.color : 'grey'}} />
-        {(this.props.active == false) && <View style={{flex: 1, backgroundColor: 'powderblue'}} />}
+        <TouchableOpacity onPress={() => this.props._toggleStripe(this.props.identity)} 
+                          style={{flex: 4, backgroundColor: this.props.color}} />
+        {(this.props.isActive == false) && <View style={{flex: 1, backgroundColor: 'powderblue'}} />}
+        {(this.props.isEnabled == false) && <View style={{flex: 1, backgroundColor: 'powderblue'}} />}
       </View>
     );
   }
